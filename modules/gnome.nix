ace8e92d505dc130d34da4efa1325dac222efd8c
Fahmi Lukistriya <lukisxyz@192.168.1.6>
2026-08-30 08:03:47 +0700

    feat(gnome): add catppuccin wallpaper as default background

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,4 +1,10 @@
 { pkgs, ... }:
+let
+  wallpaper = pkgs.runCommand "wallpaper" { } ''
+    mkdir -p $out
+    cp ${../wallpapers/wallpaper.jpg} $out/wallpaper.jpg
+  '';
+in
 {
   home.packages = with pkgs; [
     gnomeExtensions.blur-my-shell
@@ -17,6 +23,10 @@
       gtk-theme = "catppuccin-mocha";
       icon-theme = "Papirus";
       monospace-font-name = "JetBrains Mono 12";
+    };
+    "org/gnome/desktop/background" = {
+      picture-uri = "file://${wallpaper}/wallpaper.jpg";
+      picture-uri-dark = "file://${wallpaper}/wallpaper.jpg";
     };
     "org/gnome/desktop/default-applications/terminal" = {
       exec = "kitty";
