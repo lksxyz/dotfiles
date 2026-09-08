f58594bbe79384ebd98d80863fbb90dd92f57fd6
Fahmi Lukistriya <lukisxyz@192.168.1.6>
2026-09-08 10:21:37 +0700

    update omp

--- pkgs/omp/default.nix
+++ pkgs/omp/default.nix
@@ -6,11 +6,11 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "omp";
-  version = "18.0.10";
+  version = "18.1.14";
 
   src = fetchurl {
     url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/omp-linux-x64";
-    sha256 = "sha256-sT5rKnSlxx5XufcX4PxINLz+BgnzDcF4KpGXayMDYaA=";
+    sha256 = "sha256-J91my2osOf/1Hh/m20Dx83b+5KPQRFfyNu2qXQKQ5NI=";
   };
 
   dontUnpack = true;
